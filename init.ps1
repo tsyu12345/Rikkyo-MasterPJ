5ec323a61370d53d9879a08dc24b3c52dbcba185
tsyu12345 <syukut@gmail.com>
2024-05-06 20:37:05 +0900

    環境初期化用スクリプトの微修正。

--- init.ps1
+++ init.ps1
@@ -1,6 +1,6 @@
 # ml-agents release21のクローン
 Write-Host "ml-agents release21のクローンを行います。"
-git clone --branch release_21 https://github.com/Unity-Technologies/ml-agents.git
+git clone --branch develop https://github.com/Unity-Technologies/ml-agents.git
 
 Write-Host "プロジェクトの依存関係をインストールします。"
 #rye sync 
