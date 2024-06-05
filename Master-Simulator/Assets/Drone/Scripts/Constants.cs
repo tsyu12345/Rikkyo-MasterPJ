c009b778e578c7e6bb4fa48d69e4a2134d68bec2
tsyu12345 <syukut@gmail.com>
2024-06-05 17:28:07 +0900

    【ミニ環境 #3】ドローンエージェントの制御系をenumで統一

--- Master-Simulator/Assets/Drone/Scripts/Constants.cs
+++ Master-Simulator/Assets/Drone/Scripts/Constants.cs
@@ -4,4 +4,11 @@ namespace Constants {
         public const string Agent = "Agent";
         public const string Evacuee = "Evacuee";
     }
+
+    public enum DroneCtrlIndex {
+        Horizontal = 3,
+        Vertical = 2,
+        Rotation = 1,
+        Altitude = 0
+    }
 }
