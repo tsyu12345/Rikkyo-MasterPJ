c8b3f92a2d6c1636f4a27e1b930cca241933c79a
tsyu12345 <syukut@gmail.com>
2024-06-05 22:09:42 +0900

    【ミニ環境 #3】学習用スクリプトの調整。exe対応

--- learn.ps1
+++ learn.ps1
@@ -1,13 +1,23 @@
 param(
     [int]$envs= 4,
     [int]$modelId=0,
-    [bool]$force = $false
+    [bool]$force = $false,
+    [bool]$exe = $false
 )
 
 $configurePath = "Master-Simulator\config\test1_0.yaml"
+$exePath = "Master-Simulator\build\Master-Simulator.exe"
 
-if($force) {
-    mlagents-learn $configurePath --run-id=$modelId --force
+if($exe) {
+    if($force) {
+        mlagents-learn $configurePath --env=$exePath --num-envs=$envs --run-id=$modelId --force --width=1920 --height=1080
+    } else {
+        mlagents-learn $configurePath --env=$exePath --run-id=$modelId --num-envs=$envs --width=1920 --height=1080
+    }
 } else {
-    mlagents-learn $configurePath --run-id=$modelId
+    if($force) {
+        mlagents-learn $configurePath --run-id=$modelId --force --width=1920 --height=1080
+    } else {
+        mlagents-learn $configurePath --run-id=$modelId --width=1920 --height=1080
+    }
 }
