8837d06c8d5e38770d989c7c5fc2b281241b2023
tsyu1234 <syukunt@gmail.com>
2024-06-27 23:16:35 +0900

    【ミニ環境 #18】シェルスクリプト修正

--- learn.ps1
+++ learn.ps1
@@ -10,14 +10,14 @@ $exePath = "Master-Simulator\build\Master-Simulator.exe"
 
 if($exe) {
     if($force) {
-        mlagents-learn $configurePath --env=$exePath --num-envs=$envs --run-id=$modelId --force --width=1920 --height=1080
+        mlagents-learn $configurePath --env=$exePath --num-envs=$envs --run-id=$modelId --force --width=1920 --height=1080 --torch-device="cuda"
     } else {
-        mlagents-learn $configurePath --env=$exePath --run-id=$modelId --num-envs=$envs --width=1920 --height=1080
+        mlagents-learn $configurePath --env=$exePath --run-id=$modelId --num-envs=$envs --width=1920 --height=1080 --torch-device="cuda"
     }
 } else {
     if($force) {
-        mlagents-learn $configurePath --run-id=$modelId --force --width=1920 --height=1080
+        mlagents-learn $configurePath --run-id=$modelId --force --width=1920 --height=1080 --torch-device="cuda"
     } else {
-        mlagents-learn $configurePath --run-id=$modelId --width=1920 --height=1080
+        mlagents-learn $configurePath --run-id=$modelId --width=1920 --height=1080 --torch-device="cuda"
     }
 }
