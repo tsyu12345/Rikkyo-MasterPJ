93e79d467840f95a6a34e1280e7621d2a62d5b0b
tsyu12345 <syukut@gmail.com>
2024-09-01 19:32:58 +0900

    【横須賀マップ】エディタ上で避難者出現範囲を可視化。

--- Master-Simulator/Assets/Scripts/PLATEAUEnvManager.cs
+++ Master-Simulator/Assets/Scripts/PLATEAUEnvManager.cs
@@ -30,6 +30,7 @@ public class PLATEAUEnvManager : EnvManager {
 
     [Header("Evacuees Spawn Settings")]
     public float EvacueeSpawnRadius = 10.0f; // ランダム生成範囲の半径
+    public Vector3 SpawnCenter = Vector3.zero; // スポーンエリアの中心位置
     public int EvacueeSpawnMaxAttempts = 30; // 最大試行回数
     public int EvacueeSize = 100; // 避難者の数
 
@@ -38,6 +39,12 @@ public class PLATEAUEnvManager : EnvManager {
     public Slider progressBar; // プログレスバー
     //public TMP_Text progressText; // 進捗を示すテキスト
 
+    private Color gizmoColor = Color.red; // エディタ上でスポーン範囲を示す線の色
+
+    void OnDrawGizmos() {
+        Gizmos.color = gizmoColor; // Gizmoの色を設定
+        Gizmos.DrawWireSphere(SpawnCenter, EvacueeSpawnRadius); // 中心から半径のワイヤーフレームの球体を描画
+    }
     public override void Start() {
         base.Start();
         loadingPanel.SetActive(false); // 最初はローディング画面を非表示
@@ -80,33 +87,25 @@ public class PLATEAUEnvManager : EnvManager {
     }
 
     private void SpawnEvacueeOnNavMesh() {
-        for (int i = 0; i < EvacueeSpawnMaxAttempts; i++) {
-            Vector3 randomPoint = GetRandomPoint();
-            if (TryGetNavMeshPosition(randomPoint, out Vector3 navMeshPosition)) {
-                var newEvacuee = Instantiate(Evacuee, navMeshPosition, Quaternion.identity);
-                Evacuees.Add(newEvacuee);
-                newEvacuee.transform.parent = transform;
-                newEvacuee.tag = Tags.Evacuee;
-                return;
-            }
-        }
-        Debug.LogWarning("Could not find a suitable NavMesh position after maximum attempts.");
+        var spawnPos = GetRandomPositionOnNavMesh();
+        spawnPos.y = transform.position.y;
+        var newEvacuee = Instantiate(Evacuee, spawnPos, Quaternion.identity);
+        Evacuees.Add(newEvacuee);
+        newEvacuee.transform.parent = transform;
+        newEvacuee.tag = Tags.Evacuee;
     }
 
-    private Vector3 GetRandomPoint() {
-        Vector3 randomPoint = transform.position + UnityEngine.Random.insideUnitSphere * EvacueeSpawnRadius;
-        randomPoint.y = transform.position.y; // 必要に応じて高さを調整
-        return randomPoint;
-    }
-
-    private bool TryGetNavMeshPosition(Vector3 point, out Vector3 navMeshPosition) {
+    /// <summary>
+    /// ナビメッシュ上の任意の座標を取得する。
+    /// </summary>
+    /// <returns>ランダムなナビメッシュ上の座標 or Vector3.zero</returns>
+    private Vector3 GetRandomPositionOnNavMesh() {
+        Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * EvacueeSpawnRadius; // 半径内のランダムな位置を取得
+        randomDirection += SpawnCenter; // 中心位置を加算
         NavMeshHit hit;
-        if (NavMesh.SamplePosition(point, out hit, EvacueeSpawnRadius, NavMesh.AllAreas)) {
-            navMeshPosition = hit.position;
-            return true;
+        if (NavMesh.SamplePosition(randomDirection, out hit, EvacueeSpawnRadius, NavMesh.AllAreas)) {
+            return hit.position;
         }
-
-        navMeshPosition = Vector3.zero;
-        return false;
+        return Vector3.zero; // ナビメッシュが見つからなかった場合
     }
 }
